bb694c018954e5b6513981b33efb7f807401fc61
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-05-23 14:47:55 -0700

    created session for name deck players

--- main.rb
+++ main.rb
@@ -3,18 +3,40 @@ require 'sinatra'
 
 set :sessions, true
 
-get '/home' do
-	"Blackjack, coming soon! Badum!!!"
+get '/form' do
+    erb :form
+end
+
+
+get '/game' do
+  # deck
+  suits = ['H', 'D', 'C', 'S']
+  values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] 
+  session[:deck] = suits.product(values).shuffle!
+  # deal cards
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+  erb :game
+end
+
+post '/game' do
+  session[:name] = params[:name]
+  redirect '/game'
 end
 
 get '/' do
-  erb :index
+  if session[:name]
+    redirect '/game'
+  else
+    redirect '/form'
+  end
 end
 
 get '/nested_template' do
   erb :"/users/profile"
-end
-
-get '/redirect' do
-  redirect '/'
 end
