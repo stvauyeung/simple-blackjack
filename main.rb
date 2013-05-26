5fb3643fd377398b04c6581b2fc04bdd23263711
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-05-25 17:37:16 -0700

    added card images, dealer turn begin

--- main.rb
+++ main.rb
@@ -3,6 +3,51 @@ require 'sinatra'
 require 'pry'
 set :sessions, true
 
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+    arr.each do |a|
+      if a == 'A'
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+    arr.select{|element| element == 'A'}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+
+  def to_image(card)
+    case card[0]
+      when 'H' then @suit = 'hearts'
+      when 'D' then @suit = 'diamonds'
+      when 'C' then @suit = 'clubs'
+      when 'S' then @suit = 'spades'
+    end
+
+    "<img src='images/cards/#{@suit}_#{card[1]}.jpg' alt='#{card[1]} of #{@suit}'>"
+  end
+
+  def blackjack_or_bust(cards)
+    if calculate_total(cards) == 21
+      session[:player_turn] = "dealer"
+      redirect '/game'
+      @error = "#{session[:player_name]} hit 21!"
+    elsif calculate_total(cards) > 21
+      session[:player_turn] = "dealer"
+      redirect '/game'
+      @error = "#{session[:player_name]} busted."
+    end
+  end
+end
+
 get '/' do
   session.delete :player_turn
 
@@ -35,14 +80,14 @@ get '/game' do
   end
 
   if session[:player_turn] == "player"
+    blackjack_or_bust(session[:player_cards])
     erb :game
   elsif session[:player_turn] == "dealer"
-    # begin dealer turn
-    "BEGIN DEALER TURN"
+    erb :dealer
   else
     # deck
     suits = ['H', 'D', 'C', 'S']
-    values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] 
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"] 
     session[:deck] = suits.product(values).shuffle!
     # deal cards
     session[:dealer_cards] = []
