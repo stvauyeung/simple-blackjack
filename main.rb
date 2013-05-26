577223a2a43ee2ae16c995b6d23793674247132a
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-05-26 12:34:36 -0700

    updated styling and player turn functions

--- main.rb
+++ main.rb
@@ -9,14 +9,14 @@ helpers do
 
     total = 0
     arr.each do |a|
-      if a == 'A'
+      if a == 'ace'
         total += 11
       else
         total += a.to_i == 0 ? 10 : a.to_i
       end
     end
 
-    arr.select{|element| element == 'A'}.count.times do
+    arr.select{|element| element == 'ace'}.count.times do
       break if total <= 21
       total -= 10
     end
@@ -24,33 +24,23 @@ helpers do
     total
   end
 
-  def to_image(card)
+  def card_image(card)
     case card[0]
-      when 'H' then @suit = 'hearts'
-      when 'D' then @suit = 'diamonds'
-      when 'C' then @suit = 'clubs'
-      when 'S' then @suit = 'spades'
+      when 'H' then suit = 'hearts'
+      when 'D' then suit = 'diamonds'
+      when 'C' then suit = 'clubs'
+      when 'S' then suit = 'spades'
     end
 
-    "<img src='images/cards/#{@suit}_#{card[1]}.jpg' alt='#{card[1]} of #{@suit}'>"
-  end
-
-  def blackjack_or_bust(cards)
-    if calculate_total(cards) == 21
-      session[:player_turn] = "dealer"
-      redirect '/game'
-      @error = "#{session[:player_name]} hit 21!"
-    elsif calculate_total(cards) > 21
-      session[:player_turn] = "dealer"
-      redirect '/game'
-      @error = "#{session[:player_name]} busted."
-    end
+    "<img src='/images/cards/#{suit}_#{card[1]}.jpg' alt='#{card[1]} of #{suit}' class='card_image'>"
   end
 end
 
-get '/' do
-  session.delete :player_turn
+before do
+  @player_buttons = true
+end
 
+get '/' do
   if session[:player_name]
     redirect '/game'
   else
@@ -65,7 +55,7 @@ end
 post '/form' do
   if params[:player_name].nil? || params[:player_name].empty?
       @error = "You must enter a name!"
-      erb :form
+      halt erb(:form)
   else
     session[:player_name] = params[:player_name]
     redirect '/game'
@@ -79,36 +69,41 @@ get '/game' do
     redirect '/form'
   end
 
-  if session[:player_turn] == "player"
-    blackjack_or_bust(session[:player_cards])
-    erb :game
-  elsif session[:player_turn] == "dealer"
-    erb :dealer
-  else
-    # deck
-    suits = ['H', 'D', 'C', 'S']
-    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"] 
-    session[:deck] = suits.product(values).shuffle!
-    # deal cards
-    session[:dealer_cards] = []
-    session[:player_cards] = []
-    session[:dealer_cards] << session[:deck].pop
-    session[:player_cards] << session[:deck].pop
-    session[:dealer_cards] << session[:deck].pop
-    session[:player_cards] << session[:deck].pop
+  # deck
+  suits = ['H', 'D', 'C', 'S']
+  values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"] 
+  session[:deck] = suits.product(values).shuffle!
+  # deal cards
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
 
-    session[:player_turn] = "player"
-    erb :game
+  if calculate_total(session[:player_cards]) == 21
+    @success = "You hit blackjack!"
+    @player_buttons = false
   end
+
+  erb :game
 end
 
-post '/game' do
-  # actions for player using turn buttons
-  if params[:player_action] == 'hit'
-    session[:player_cards] << session[:deck].pop
-  elsif params[:player_action] == 'stay'
-    session[:player_turn] = "dealer"
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  if calculate_total(session[:player_cards]) > 21
+    @error = "Sorry, you have busted."
+    @player_buttons = false
+  elsif calculate_total(session[:player_cards]) == 21
+    @success = "You hit twenty-one!"
+    @player_buttons = false
   end
 
-  redirect '/game'
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} stays."
+  @player_buttons = false
+  erb :game
 end
